10d39e899bb317ceec87be0084638f6ce8b49018
Audrey Roselyn Natasya <152130643+AudreyNatasya@users.noreply.github.com>
2023-12-03 18:56:18 +0700

    Add files via upload

---
+++ storage/framework/views/294d9bc4a2ec19f0af62f6d73215ac0888f95d84.php
@@ -0,0 +1,182 @@
+
+<?php $__env->startSection('title', 'List User'); ?>
+
+<?php $__env->startSection('content'); ?>
+<section class="section custom-section">
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <h4>List User</h4>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="nav-icon fas fa-folder-plus"></i>&nbsp; Tambah Data User</button>
+                    </div>
+                    <div class="card-body">
+                        <?php if($message = Session::get('success')): ?>
+                        <div class="alert alert-success alert-dismissible show fade">
+                            <div class="alert-body">
+                                <button class="close" data-dismiss="alert">
+                                    <span>&times;</span>
+                                </button>
+                                <?php echo e($message); ?>
+
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <?php if($message = Session::get('error')): ?>
+                        <div class="alert alert-danger alert-dismissible show fade">
+                            <div class="alert-body">
+                                <button class="close" data-dismiss="alert">
+                                    <span>&times;</span>
+                                </button>
+                                <?php echo e($message); ?>
+
+                            </div>
+                        </div>
+                        <?php endif; ?>
+                        <div class="table-responsive">
+                            <table class="table table-striped" id="table-2">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama User</th>
+                                        <th>Roles</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $__currentLoopData = $user; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $result => $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <tr>
+                                        <td><?php echo e($loop->iteration); ?></td>
+                                        <td><?php echo e($data->name); ?></td>
+                                        <td><?php echo e($data->roles); ?></td>
+                                        <td>
+                                            <div class="d-flex">
+                                                <form method="POST" action="<?php echo e(route('user.destroy', $data->id)); ?>">
+                                                    <?php echo csrf_field(); ?>
+                                                    <?php echo method_field('delete'); ?>
+                                                    <button class="btn btn-danger btn-sm show_confirm" data-toggle="tooltip" title='Delete' style="margin-left: 8px"><i class="nav-icon fas fa-trash-alt"></i> &nbsp; Hapus</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" tabindex="-1" role="dialog" id="exampleModal">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Tambah User</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="<?php echo e(route('user.store')); ?>" method="POST">
+                                <?php echo csrf_field(); ?>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <?php if($errors->any()): ?>
+                                        <div class="alert alert-danger alert-dismissible show fade">
+                                            <div class="alert-body">
+                                                <button class="close" data-dismiss="alert">
+                                                    <span>&times;</span>
+                                                </button>
+                                                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                <?php echo e($error); ?>
+
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                            </div>
+                                        </div>
+                                        <?php endif; ?>
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input type="email" id="email" name="email" class="form-control <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" placeholder="<?php echo e(__('Email User')); ?>" value="<?php echo e(old('email')); ?>">
+                                        </div>
+                                        <input name="password" type="password" value="password123" hidden>
+                                        <div class="form-group">
+                                            <label for="roles">Roles</label>
+                                            <select id="roles" name="roles" class="select2 form-control <?php $__errorArgs = ['roles'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>">
+                                                <option value="">-- Pilih Roles --</option>
+                                                <option value="admin">Admin</option>
+                                                <option value="guru">Guru</option>
+                                                <option value="siswa">Siswa</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group" id="noId"></div>
+                                    </div>
+                                </div>
+                                <div class="modal- br">
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startPush('script'); ?>
+<script type="text/javascript">
+    $('.show_confirm').click(function(event) {
+        var form = $(this).closest("form");
+        var name = $(this).data("name");
+        event.preventDefault();
+        swal({
+                title: `Yakin ingin menghapus data ini?`
+                , text: "Data akan terhapus secara permanen!"
+                , icon: "warning"
+                , buttons: true
+                , dangerMode: true
+            , })
+            .then((willDelete) => {
+                if (willDelete) {
+                    form.submit();
+                }
+            });
+    });
+
+</script>
+<script>
+    $(document).ready(function() {
+        $('#roles').change(function() {
+            var kel = $('#roles option:selected').val();
+            if (kel == "guru") {
+                $("#noId").html('<label for="nip">NIP guru</label><input id="nip" type="text" onkeypress="return inputAngka(event)" placeholder="NIP Guru" class="form-control" name="nip" autocomplete="off">');
+            } else if (kel == "siswa") {
+                $("#noId").html(`<label for="nis">NIS Siswa</label><input id="nis" type="text" placeholder="NIS Siswa" class="form-control" name="nis" autocomplete="off">`);
+            } else if (kel == "admin") {
+                $("#noId").html(`<label for="name">Nama Admin</label><input id="name" type="text" placeholder="Nama admin" class="form-control" name="name" autocomplete="off">`);
+            } else {
+                $("#noId").html("")
+            }
+        });
+    });
+
+</script>
+<?php $__env->stopPush(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Audrey SI Sekolah\Sistem-Informasi-Sekolah\resources\views/pages/admin/user/index.blade.php ENDPATH**/ ?>
